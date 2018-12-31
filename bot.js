723b04bceab95f092949d34b2d947af95f620da0
OnurBeyTR <46277308+OnurBeyTR@users.noreply.github.com>
2018-12-31 16:21:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,7 +49,7 @@ client.reload = command => {
   });
 };
 
-let statuses = ['discord.gg/UDD2Sm', 'Selam'];
+let statuses = ['discord.gg/UDD2Sm', '?yardım ile bot hakkında yardım alabilirsin', 'Coded By **OnurBeyTR**'];
 client.on('ready', () => {
 	
 	setInterval(function() {
